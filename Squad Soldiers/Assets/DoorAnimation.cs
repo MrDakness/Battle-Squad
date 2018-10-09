11663e7a3c0a9ed33b0cfdbac704f4c34e43f619
MrDakness <43968857+MrDakness@users.noreply.github.com>
2018-10-09 20:37:24 +0200

    Made the hole house

---
+++ Squad Soldiers/Assets/DoorAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour {
+
+    public Animator DoorOpenandCloseAnimation;
+
+	// Use this for initialization
+	void Start () {
+        DoorOpenandCloseAnimation = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("f"))
+        {
+            DoorOpenandCloseAnimation.Play("Door Open Animation");
+        }
+	}
+}
