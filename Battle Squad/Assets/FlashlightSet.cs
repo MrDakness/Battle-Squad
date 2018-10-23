6cf867ba8d00befdd27b5c96f0002d30f8e6846c
MrDakness <43968857+MrDakness@users.noreply.github.com>
2018-10-23 15:51:05 +0200

    Alot of changes

---
+++ Battle Squad/Assets/FlashlightSet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightSet : MonoBehaviour {
+
+    public GameObject Flashlight;
+
+    public bool FlashLight;
+
+    void Start()
+    {
+        Flashlight = gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("g"))
+        {
+            Flashlight.SetActive(true);
+        }
+
+        if (Input.GetKeyDown("g"))
+        {
+            Flashlight.SetActive(false);
+        }
+    }
+}
---
+++ Battle Squad/Assets/MovementAnimationAR3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAnimationAR3 : MonoBehaviour {
+
+    public Animator AR3Animations;
+
+	// Use this for initialization
+	void Start () {
+        AR3Animations = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("c"))
+        {
+            AR3Animations.Play("");
+        }
+	}
+}
