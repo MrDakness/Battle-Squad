4a53489d9c254583199281e408860cc9e486f7c7
MrDakness <43968857+MrDakness@users.noreply.github.com>
2018-10-24 20:28:23 +0200

    Have made some changes

---
+++ Battle Squad/Assets/Maps/Crimea/Scripts/Elivator/ElevatorScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour
+{
+
+    private Animator _animator;
+
+    public GameObject OpenPanel = null;
+
+    private bool _isInsideTrigger = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = transform.Find("PTK_Elevator_2Floors").GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _isInsideTrigger = true;
+            OpenPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _isInsideTrigger = false;
+            _animator.SetBool("open", false);
+            OpenPanel.SetActive(false);
+        }
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (IsOpenPanelActive && _isInsideTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                OpenPanel.SetActive(false);
+                _animator.SetBool("open", true);
+            }
+        }
+    }
+}
