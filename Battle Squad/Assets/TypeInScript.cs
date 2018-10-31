fe34457b32e6200bb946c1bf2ea57b87044c77c7
MrDakness <43968857+MrDakness@users.noreply.github.com>
2018-10-31 22:26:32 +0100

    Made some changes

---
+++ Battle Squad/Assets/TypeInScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections
+
+public class AutoType : MonoBehaviour
+{
+
+    public float letterPause = 0.2f;
+
+    string message;
+
+    // Use this for initialization
+    void Start()
+    {
+        message = guiText.text;
+        guiText.text = "Development";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in message.ToCharArray())
+        {
+            guiText.text += letter;
+            yield return 0;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+}
