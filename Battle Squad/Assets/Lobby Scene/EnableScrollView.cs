c6a547cdd180ff1fbeebd16aac090776e21bc4d2
MrDakness <43968857+MrDakness@users.noreply.github.com>
2018-11-04 15:17:47 +0100

    Have updated the credits list

---
+++ Battle Squad/Assets/Lobby Scene/EnableScrollView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableScrollView : MonoBehaviour {
+
+    public GameObject objectToActivate;
+
+    public GameObject objectToDisable;
+
+    private void Start()
+    {
+        StartCoroutine(ActivationRoutine());
+    }
+
+    private IEnumerator ActivationRoutine()
+    {
+        //Wait for 0.1 secs.
+        yield return new WaitForSeconds(5);
+
+        //Turn My game object that is set to false(off) to True(on).
+        objectToActivate.SetActive(true);
+        objectToDisable.SetActive(false);
+    }
+}
