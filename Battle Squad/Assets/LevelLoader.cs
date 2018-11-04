c56f43f3abf27877a50127090afe7f91fb2b3ca5
MrDakness <43968857+MrDakness@users.noreply.github.com>
2018-11-04 19:50:02 +0100

    Worked on alot of stuff with the main menu and some on the campaign scene

---
+++ Battle Squad/Assets/LevelLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+    public GameObject LoadingScreen;
+    public Slider LoadingSlider;
+    public Text progressText;
+
+	public void LoadCampaign (int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            LoadingSlider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+
+}
