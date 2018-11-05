8938c773419e2bfb0858c4ea9eeedec331aa160b
MrDakness <43968857+MrDakness@users.noreply.github.com>
2018-11-05 20:41:31 +0100

    Have made some small changes to the menu

--- Battle Squad/Assets/Lobby Scene/EnableScrollView.cs
+++ Battle Squad/Assets/Lobby Scene/EnableScrollView.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class EnableScrollView : MonoBehaviour {
 
-    public GameObject objectToActivate;
+    public AudioSource MenuMusicAudioSource;
 
-    public GameObject objectToDisable;
+    public AudioClip MenuMusic;
 
     private void Start()
     {
         StartCoroutine(ActivationRoutine());
+
+        MenuMusicAudioSource.clip = MenuMusic;
     }
 
     private IEnumerator ActivationRoutine()
     {
         //Wait for 0.1 secs.
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(14);
 
         //Turn My game object that is set to false(off) to True(on).
-        objectToActivate.SetActive(true);
-        objectToDisable.SetActive(false);
+        MenuMusicAudioSource.Play();
     }
 }
